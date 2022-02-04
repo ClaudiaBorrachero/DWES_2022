3fb23b3210b32dcb90c549f5193bd768bf835963
ClaudiaBorrachero <alum.cnereaborracherom@iesalixar.org>
2022-02-04 17:15:12 +0100

    DWES

---
+++ U5_HIBERNATE/PRACTICAS_CLASE/demoSpring/src/main/java/org/iesalixar/controller/GreetingController.java
@@ -0,0 +1,26 @@
+package org.iesalixar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/user/")
+public class GreetingController {
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name = "name", required = false, defaultValue="World") String name, Model model) {
+		
+		model.addAttribute("name", name);
+		
+		return "greeting";
+	}
+	
+	@GetMapping("/bye")
+	public String bye(Model model) {
+		
+		return "bye";
+	}
+}
